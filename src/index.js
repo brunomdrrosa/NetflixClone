ed5a54fe6c2f7001f86f82930b8c5cc1840ac94c
Bruno Machado <brunomdr46@gmail.com>
2023-01-02 16:36:45 -0300

    React Context for Firebase

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 import { GlobalStyles } from './globalStyles';
 import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>
 );
