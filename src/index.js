94affa2bf5e2ac5fc4467a7abe6bc1e5b6891648
Bruno Machado <brunomdr46@gmail.com>
2023-01-02 16:09:03 -0300

    Firebase connection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import { GlobalStyles } from './globalStyles';
+import { firebase } from './lib/firebase.prod';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
