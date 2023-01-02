b31e8133b406d7638eb83dc9b258b6e0f3636fd7
Bruno Machado <brunomdr46@gmail.com>
2023-01-02 08:30:28 -0300

    Creating React Router Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp } from './pages';
+
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path="/browse">
+        <Browse />
+      </Route>
+      <Route exact path="/signin">
+        <SignIn />
+      </Route>
+      <Route exact path="/signup">
+        <SignUp />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
